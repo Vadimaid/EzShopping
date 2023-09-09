367430aac1455077d32df7ab30000d3bf12e15c6
vadim <vadimaid@gmail.com>
2023-09-09 20:59:47 +0600

    Initial commit

---
+++ src/main/java/kg/ezshopping/ezshopping/EzShoppingApplication.java
@@ -0,0 +1,13 @@
+package kg.ezshopping.ezshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzShoppingApplication.class, args);
+	}
+
+}
---
+++ src/main/java/kg/ezshopping/ezshopping/types/UserType.java
@@ -0,0 +1,7 @@
+package kg.ezshopping.ezshopping.types;
+
+public enum UserType {
+    SYSTEM,
+    SHOP,
+    CLIENT
+}
