7ae3e71ffb3450a761d940cee2c0cce28257fd9e
Akmaral Kaiypberdieva <yamaralista1@gmail.com>
2023-09-14 04:17:30 +0600

    add exceptions

---
+++ src/main/java/kg/ezshopping/ezshopping/exception/StoreAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package kg.ezshopping.ezshopping.exception;
+
+public class StoreAlreadyExistsException extends Exception{
+    public StoreAlreadyExistsException(String message) {
+        super(message);
+    }
+}
